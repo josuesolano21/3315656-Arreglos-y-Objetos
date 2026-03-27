9b2a49ce042848af42b22cc07800b449936a6c2c
andres dulcey <andresdulcey417@gmail.com>
2026-03-27 17:56:48 -0500

    realize ejercicios 6 y 7 de transferencia

--- Actividades_transferencia_js/ejercicio_7.js
+++ Actividades_transferencia_js/ejercicio_7.js
@@ -1,2 +1,9 @@
 /*7. Control de edades: Cree un arreglo con edades de cinco personas. Recorra el
 arreglo usando for...of y muestre solo las edades mayores o iguales a 18.*/
+
+let edades = [18, 21, 26, 31];
+for (const edad of edades) {
+  if (edad >= edad) {
+    console.log(edad);
+  }
+}
