9598da88dca0d96a8bcd59e608712b5808f99ebb
andres dulcey <andresdulcey417@gmail.com>
2026-04-03 10:10:27 -0500

    realize los ejercicios 8 9 y 10 de transferencia

--- Actividades_transferencia_js/ejercicio_10.js
+++ Actividades_transferencia_js/ejercicio_10.js
@@ -5,3 +5,36 @@ a. Lista de colores favoritos.
 b. Información completa de un estudiante.
 c. Precios de diferentes camisetas.
 d. Descripción de un computador portátil.*/
+
+let colores = ["rojo", "azul", "verde", "negro"];
+console.log(colores);
+//a) Tipo: Arreglo Justificación: Porque es una colección de varios elementos del mismo tipo
+
+let estudiante = {
+    nombre: "andres",
+    edad: 21,
+    carrera: "Ingeniería",
+    promedio: 4.5
+};
+
+console.log(estudiante);
+//b) Tipo: Objeto Justificación: Porque contiene diferentes datos relacionados a una sola entidad.
+
+let camisetas = [
+    { nombre: "Camiseta blanca", precio: 30000 },
+    { nombre: "Camiseta negra", precio: 35000 },
+    { nombre: "Camiseta roja", precio: 32000 }
+];
+
+console.log(camisetas);
+//c) Tipo: Arreglo (de objetos) Justificación: Porque son varios productos, cada uno con su propio precio
+
+let computador = {
+    marca: "HP",
+    procesador: "Intel i5",
+    ram: "8GB",
+    almacenamiento: "512GB SSD"
+};
+
+console.log(computador);
+//d) Tipo: Objeto Justificación: Porque describe las características de un solo elemento
